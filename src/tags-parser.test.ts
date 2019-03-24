a2b9d0447082f418d13e8ac49d06ee14c4c24752
hdorgeval <henri.dorgeval@outlook.fr>
2019-03-24 02:23:41 +0100

    test(parser): add test for tag parsing

--- src/tags-parser.test.ts
+++ src/tags-parser.test.ts
@@ -3,7 +3,7 @@ import 'jest';
 import { join } from 'path';
 import { tagsFromDescription } from './tags-parser';
 
-afterEach(() => {
+afterAll(() => {
   unlinkSync(join(process.cwd(), 'testcafe-reporter-cucumber-json.json'));
 });
 
@@ -16,4 +16,19 @@ test('It should ignore words with less than 3 characters', () => {
 
   // Then
   expect(result.includes('yo')).toBe(false);
+  expect(result.includes('it')).toBe(false);
+  expect(result.includes('@foo')).toBe(true);
+  expect(result.includes('@bar')).toBe(true);
+});
+
+test("It should prefix tags with the '@' character", () => {
+  // Given
+  const description = 'it should ignore yo and it but not foo and bar';
+
+  // When
+  const result = tagsFromDescription(description).map((tag) => tag.name);
+
+  // Then
+  expect(result.includes('@foo')).toBe(true);
+  expect(result.includes('@bar')).toBe(true);
 });
