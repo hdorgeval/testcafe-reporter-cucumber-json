7f0937d4739538ad4cbc65c0f04aac6103edb06f
hdorgeval <henri.dorgeval@outlook.fr>
2020-09-05 18:20:37 +0200

    feat(reporter): be able to set cli options through configuration file

--- src/command-line-args.ts
+++ src/command-line-args.ts
@@ -1,17 +1,21 @@
+import { options } from './options';
 import minimist, { ParsedArgs } from 'minimist';
 const args: ParsedArgs = minimist(process.argv.slice(2));
+const optionsFromConfigurationFile = options;
 
 export const cliArgs = {
   ...args,
-  appName: args['reporter-app-name'],
-  appVersion: args['reporter-app-version'],
-  reportFolder: args['reporter-json-folder'],
+  appName: args['reporter-app-name'] || optionsFromConfigurationFile.args?.appName,
+  appVersion:
+    args['reporter-app-version'] || optionsFromConfigurationFile.args?.appVersion,
+  reportFolder:
+    args['reporter-json-folder'] || optionsFromConfigurationFile.args?.reportFolder,
   rawCommandLine: process.argv.join(' '),
 } as CliArgs;
 
 export interface CliArgs extends ParsedArgs {
-  appName: string;
-  appVersion: string;
-  reportFolder: string;
+  appName: string | false;
+  appVersion: string | false;
+  reportFolder: string | false;
   rawCommandLine: string;
 }
--- src/default-options.ts
+++ src/default-options.ts
@@ -1,8 +1,17 @@
+import { CliArgs } from './command-line-args';
+
 export interface ReporterOption {
   noisyTags: string[];
   verbose: boolean;
+
+  args?: Partial<CliArgs>;
 }
 export const defaultOptions: ReporterOption = {
+  args: {
+    appName: false,
+    appVersion: false,
+    reportFolder: 'cucumber-json-reports',
+  },
   noisyTags: [
     '(t)',
     'able',
